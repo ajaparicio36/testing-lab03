85e1aec9ec123c01a6945ae918acdc4be22926ea
ANTHONY JOHN APARICIO <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-04-03 00:00:24 +0800

    build: add colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
     },
+    colors: {
+      'background': '#f8f5f9',
+      'primary': '#9853b3',
+      'white': '#f8f5f9',
+      'gray': '#d1d5db',
+      'secondary': '#c793dc',
+      'accent': '#b65bd9'
+    },
   },
   plugins: [],
 }
