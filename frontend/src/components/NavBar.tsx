7519261726ff311fa61a68d889eaee594aadff03
ANTHONY JOHN APARICIO <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-04-03 00:01:11 +0800

    refactor: change routing

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,29 +1,33 @@
 import React, { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import Icon from "@mdi/react";
-
 import { mdiPh, mdiAccount, mdiCash } from "@mdi/js";
 
 const NavBar = () => {
   const navigate = useNavigate();
+
   return (
     <Fragment>
-      <div className="flex w-full h-12 bg-sky-300 px-4 items-center font-poppins justify-between">
-        <div className="text-md lg:ml-4" onClick={() => navigate("/")}>
-          <Icon path={mdiPh} size={2} color="black" />
+      <nav className="flex w-full h-12 bg-background px-4 items-center font-poppins justify-between border-b border-gray">
+        <div
+          className="text-md lg:ml-4 cursor-pointer"
+          onClick={() => navigate("/")}
+        >
+          <Icon path={mdiPh} size={2} color="#9853b3" />
         </div>
         <div className="flex flex-row text-md mr-2 lg:mr-8">
-          <div className="flex gap-2 mr-4 items-center">
-            <Icon path={mdiCash} size={1.5} color="black" />
+          <div className="flex gap-2 mr-4 items-center text-primary">
+            <Icon path={mdiCash} size={1.5} color="#9853b3" />
             <span>Balance</span>
           </div>
-          <div className="border border-black rounded-full p-0.5">
-            <span onClick={() => navigate("/account")}>
-              <Icon path={mdiAccount} size={1.25} color="black" />
-            </span>
+          <div
+            className="border border-primary rounded-full p-0.5 cursor-pointer"
+            onClick={() => navigate("/account")}
+          >
+            <Icon path={mdiAccount} size={1.25} color="#9853b3" />
           </div>
         </div>
-      </div>
+      </nav>
     </Fragment>
   );
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -19,10 +19,10 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<App />}>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
           <Route path="/pogs/:id" element={<Pogs />} />
           <Route path="/account" element={<Account />} />
         </Route>
