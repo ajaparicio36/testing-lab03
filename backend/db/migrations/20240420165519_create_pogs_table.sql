bcbcdd549cdc9872ed11906db296d61472684bca
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-04-21 02:06:33 +0800

    feat: db migrations

---
+++ backend/db/migrations/20240420165519_create_pogs_table.sql
@@ -0,0 +1,27 @@
+-- migrate:up
+CREATE TABLE Pogs(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR NOT NULL,
+  symbol VARCHAR NOT NULL,
+  current_price NUMERIC NOT NULL,
+  previous_price NUMERIC NOT NULL DEFAULT 0,
+  color VARCHAR NOT NULL,
+  CONSTRAINT previous_price_default CHECK (previous_price = 0 OR previous_price = current_price)
+);
+
+CREATE OR REPLACE FUNCTION update_previous_price()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.previous_price := OLD.current_price;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_previous_price_trigger
+BEFORE UPDATE ON Pogs
+FOR EACH ROW
+EXECUTE FUNCTION update_previous_price();
+
+
+-- migrate:down
+DROP TABLE Pogs;
---
+++ backend/db/migrations/20240420165719_create_users_table.sql
@@ -0,0 +1,12 @@
+-- migrate:up
+CREATE TABLE Users(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    type VARCHAR NOT NULL DEFAULT 'user',
+    funds FLOAT NOT NULL DEFAULT 300.00
+);
+
+-- migrate:down
+DROP TABLE Users
