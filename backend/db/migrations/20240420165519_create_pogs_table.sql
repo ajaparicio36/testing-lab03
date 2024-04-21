1dfddc075dd0f7f441e41898eb0fceb2c6a60cae
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-04-21 18:24:49 +0800

    refactor: change pogs table

--- backend/db/migrations/20240420165519_create_pogs_table.sql
+++ backend/db/migrations/20240420165519_create_pogs_table.sql
@@ -1,12 +1,14 @@
 -- migrate:up
-CREATE TABLE Pogs(
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR NOT NULL,
-  symbol VARCHAR NOT NULL,
-  current_price NUMERIC NOT NULL,
-  previous_price NUMERIC NOT NULL DEFAULT 0,
-  color VARCHAR NOT NULL,
-  CONSTRAINT previous_price_default CHECK (previous_price = 0 OR previous_price = current_price)
+CREATE TABLE Pogs (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR NOT NULL,
+    symbol VARCHAR NOT NULL,
+    current_price NUMERIC NOT NULL,
+    previous_price NUMERIC NOT NULL DEFAULT 0,
+    color VARCHAR NOT NULL,
+    owner INTEGER,
+    CONSTRAINT fk_owner FOREIGN KEY (owner) REFERENCES Users(id),
+    CONSTRAINT previous_price_default CHECK (previous_price = 0 OR previous_price = current_price)
 );
 
 CREATE OR REPLACE FUNCTION update_previous_price()
