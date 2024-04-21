db9ba586bbca25e643fd64ae78ae44ac122242a2
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-04-21 18:25:13 +0800

    build: change tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
     },
     colors: {
-      'background': '#f8f5f9',
-      'primary': '#9853b3',
-      'white': '#f8f5f9',
-      'gray': '#d1d5db',
-      'secondary': '#c793dc',
-      'accent': '#b65bd9'
+      background: "#f8f5f9",
+      primary: "#9853b3",
+      white: "#f8f5f9",
+      gray: "#d1d5db",
+      secondary: "#c793dc",
+      accent: "#b65bd9",
     },
   },
   plugins: [],
-}
+};
